6bedc8b5d9abbf5273bd3a68fe436893c43a0408
Alex McCoy <abmccoy17@gmail.com>
2021-01-26 09:04:18 -0500

    Second Commit

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const container = document.querySelector('#container');
 const squaresInput = document.querySelector('#squaresInput');
 const changeGridBtn = document.querySelector('#changeGridBtn');
+const blackBtn = document.querySelector('#blackBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
+const clearBtn = document.querySelector('#clearBtn');
 
-let currentInputValue = 0;
+let currentInputValue = 16;
 
-//Function to program the color of the shaded squares
-function shadeRows () {
+//Function to program the color of the shaded squares to black
+function shadeBlack () {
   container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "black";
+  });
+}
+
+function shadeRainbow() {
+  container.addEventListener("mouseover", (e) => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    e.target.style.backgroundColor = randomColor;
+  })
+}
+
+function eraseColor() {
+  container.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "white";
   });
 }
 
@@ -22,22 +39,37 @@ function makeGrid(rows, cols, colorFunct) {
   }
 }
 //Inital function call to set up grid
-makeGrid(16, 16, shadeRows());
+makeGrid(16, 16, shadeBlack());
 
 //Event listener to change the layout of the grid
 changeGridBtn.addEventListener('click', () => {
   while (container.lastElementChild) {
     container.removeChild(container.lastElementChild);
   }
-  if (`${squaresInput.value}` >= 4 && `${squaresInput.value}` <= 33) {
-    makeGrid(`${squaresInput.value}`, `${squaresInput.value}`, shadeRows());
+  if (`${squaresInput.value}` <= 100 && `${squaresInput.value}` >= 1) {
+    makeGrid(`${squaresInput.value}`, `${squaresInput.value}`, shadeBlack());
     currentInputValue = parseInt(`${squaresInput.value}`);
   } else {
-    alert('Please enter a number larger than or equal to 4 or a number smaller than or equal to 33');
-    makeGrid(`${currentInputValue}`, `${currentInputValue}`, shadeRows());
+    alert("Please enter a number between 1 and 100");
+    makeGrid(`${currentInputValue}`, `${currentInputValue}`, shadeBlack());
   }
 });
 
+
+
+function clearColors() {
+  const gridItems = document.querySelectorAll(".grid-item");
+  console.log(gridItems);
+  for (item of gridItems) {
+      item.style.setProperty('background-color', 'white');
+  }
+  console.log(item.backgroundColor)
+}
+
+rainbowBtn.addEventListener('click', shadeRainbow);
+blackBtn.addEventListener('click', shadeBlack);
+eraseBtn.addEventListener('click', eraseColor);
+clearBtn.addEventListener('click', clearColors);
 
 
 
